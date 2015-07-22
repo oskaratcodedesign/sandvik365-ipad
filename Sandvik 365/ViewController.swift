cde86c9798d654755791b8f31d736c6a2f6f10c7
Karl Söderström <karl@karlsoderstrom.com>
2015-07-22 10:33:50 +0200

    Initial Commit

---
+++ Sandvik 365/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sandvik 365
+//
+//  Created by Karl Söderström on 22/07/15.
+//  Copyright (c) 2015 Commind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
