94d0917caedc6cebf139e2e4a26a693b0d0562cc
Oskar <oskar@commind.se>
2015-08-11 15:50:58 +0200

    initial roi selection controllers

---
+++ Sandvik 365/RoiSelectionViewController.swift
@@ -0,0 +1,101 @@
+//
+//  RoiSelectionViewController.swift
+//  Sandvik 365
+//
+//  Created by Oskar Hakansson on 11/08/15.
+//  Copyright (c) 2015 Commind. All rights reserved.
+//
+
+import UIKit
+
+class RoiSelectionViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    private var pageViewController: UIPageViewController?
+    private let numberOfItems = 5;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadPageController()
+        setupPageControl()
+    }
+    
+    private func loadPageController()
+    {
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("RoiPageController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        let firstController = getItemController(0)!
+        let startingViewControllers: NSArray = [firstController]
+        pageController.setViewControllers(startingViewControllers as [AnyObject], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor(red: 0.082, green:0.678, blue:0.929, alpha:1.000)
+        //appearance.backgroundColor = UIColor.darkGrayColor()
+    }
+    
+    private func getItemController(itemIndex: Int) -> RoiSelectionContentViewController? {
+        
+        if itemIndex < numberOfItems {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("RoiSelectionContentViewController") as! RoiSelectionContentViewController
+            pageItemController.itemIndex = itemIndex
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! RoiSelectionContentViewController
+        
+        if itemController.itemIndex+1 < numberOfItems {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! RoiSelectionContentViewController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return numberOfItems
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
