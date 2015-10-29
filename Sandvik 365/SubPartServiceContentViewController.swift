f192553b308d300e1d058531e08fdf6cb16ab53e
Oskar <oskar@commind.se>
2015-10-29 13:09:46 +0100

    parse out parts

--- Sandvik 365/SubPartServiceContentViewController.swift
+++ Sandvik 365/SubPartServiceContentViewController.swift
@@ -25,6 +25,48 @@ class SubPartServiceContentViewController: UIViewController {
             }
             titleLabel.text = title
         }
+        if let html = content.objectForKey("content") as? NSArray {
+            for part in html {
+                if let type = part.objectForKey("type") as? String {
+                    if type == "lead", let value = part.objectForKey("value") as? String {
+                        addLead(value)
+                    }
+                    else if type == "body", let value = part.objectForKey("value") as? String {
+                        addBody(value)
+                    }
+                    else if type == "key-feature-list", let value = part.objectForKey("value") as? NSDictionary {
+                        addKeyFeatureList(value)
+                    }
+                    else if type == "columns", let value = part.objectForKey("value") as? NSDictionary {
+                        addColumns(value)
+                    }
+                    else if type == "tabbed-content", let value = part.objectForKey("value") as? NSDictionary {
+                        addTabbedContent(value)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func addLead(string: String) {
+        
+        //add picture
+    }
+    
+    private func addBody(string: String) {
+        
+    }
+    
+    private func addKeyFeatureList(dic: NSDictionary) {
+        
+    }
+    
+    private func addColumns(dic: NSDictionary) {
+        
+    }
+    
+    private func addTabbedContent(dic: NSDictionary) {
+        
     }
 
     override func didReceiveMemoryWarning() {
