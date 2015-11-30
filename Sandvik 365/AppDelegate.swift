d3a7d55e9438a0f2465f8a353baf4b7fb1d591d6
Oskar <oskar@commind.se>
2015-11-30 10:02:14 +0100

    check for update on enter foreground

--- Sandvik 365/AppDelegate.swift
+++ Sandvik 365/AppDelegate.swift
@@ -86,6 +86,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        JSONManager().checkforUpdate({ (success, lastModified) -> () in
+            print("checkforUpdate. Last modified: %@", lastModified)
+        })
         showLoadingView()
     }
 
