7e92710e6fb88eed6ca0276cda2d036c94599381
Oskar <oskar@commind.se>
2016-01-12 10:48:33 +0100

    add contact us

---
+++ Sandvik 365/ContactUsView.swift
@@ -0,0 +1,14 @@
+//
+//  ContactUsView.swift
+//  Sandvik 365
+//
+//  Created by Oskar Hakansson on 12/01/16.
+//  Copyright © 2016 Commind. All rights reserved.
+//
+
+import Foundation
+import NibDesignable
+
+@IBDesignable class ContactUsView : NibDesignable {
+
+}
