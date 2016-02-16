63f6cb8f4a6019325fec7944b29f67c978e252c9
Oskar <oskar@commind.se>
2016-02-16 21:05:05 +0100

    style tutorial button

--- Sandvik 365/DisclaimerViewController.swift
+++ Sandvik 365/DisclaimerViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DisclaimerViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var disclaimerTextView: UITextView!
     
+    @IBOutlet weak var tutorialButton: UIButton!
     @IBOutlet weak var updateButton: UIButton!
     
     override func viewDidLoad() {
@@ -20,6 +21,7 @@ class DisclaimerViewController: UIViewController, UITextViewDelegate {
             NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleNone.rawValue]
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "checkUpdateAvailble", name: JSONManager.updateAvailable, object: nil)
         
+        self.tutorialButton.titleLabel!.textAlignment = .Center
         self.updateButton.layer.borderWidth = 2
         self.updateButton.titleLabel!.numberOfLines = 0
         self.updateButton.titleLabel!.textAlignment = .Center
