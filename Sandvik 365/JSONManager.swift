618418fc7cc6fa1e372462d4b7326f1831d2415e
Oskar <oskar@commind.se>
2016-03-17 11:54:52 +0100

    download service images

--- Sandvik 365/JSONManager.swift
+++ Sandvik 365/JSONManager.swift
@@ -12,6 +12,7 @@ import Async
 class JSONManager {
     static let updateAvailable = "updateAvailableKey"
     static let newDataAvailable = "newDataAvailableKey"
+    static let serviceHandlerImageKey = "serviceHandlerImageKey"
     private let endPoints: [EndPoint] = [.CONTENT_URL, .FIRESUPPRESSION_URL, .CONTACT_US]
     
     enum EndPoint {
@@ -305,6 +306,25 @@ class JSONManager {
                         else if let content = partService.content {
                             for image in content.images {
                                 downloadImage(baseUrl, imageUrl: image)
+                            }
+                        }
+                    }
+                }
+                /* serviceHandlerImages */
+                if let serviceHandlerImages = data.objectForKey("serviceHandlerImages") as? NSDictionary {
+                    var scale = UIScreen.mainScreen().scale
+                    if scale >= 2 {
+                        if UIScreen.mainScreen().bounds.size.height == 1366 || UIScreen.mainScreen().bounds.size.width == 1366 {
+                            //ipadpro
+                            scale = 3
+                        }
+                    }
+                    for (key, obj) in serviceHandlerImages {
+                        if let k = key as? String where (k.rangeOfString("x"+String("scale")) != nil) {
+                            if let image = obj.objectForKey("encodedUrl") as? String, let imageUrl = NSURL(string: image) {
+                                NSUserDefaults.standardUserDefaults().setURL(imageUrl, forKey: JSONManager.serviceHandlerImageKey)
+                                NSUserDefaults.standardUserDefaults().synchronize()
+                                downloadImage(baseUrl, imageUrl: imageUrl)
                             }
                         }
                     }
